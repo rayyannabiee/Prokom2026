45fe59a87ab363bc1db1d62567c7aee3c3777fbb
Verizkaprimallea <verizkaaprimalleaa@gmail.com>
2026-02-20 11:38:03 +0700

    Update notifikasi.java

--- notifikasi.java
+++ notifikasi.java
@@ -1,3 +1,29 @@
-public class notifikasi {
+class Notifikasi {
+    private String pesan;
+    private String tanggal;
+    private boolean sudahDibaca;
 
+    public Notifikasi(String pesan, String tanggal) {
+        this.pesan = pesan;
+        this.tanggal = tanggal;
+        this.sudahDibaca = false; 
+    }
+
+    public void tandaiSudahDibaca() {
+        sudahDibaca = true;
+    }
+
+    public void tampilkanNotifikasi() {
+        System.out.println("=== NOTIFIKASI PERPUSTAKAAN DIGITAL ===");
+        System.out.println("Pesan   : " + pesan);
+        System.out.println("Tanggal : " + tanggal);
+        
+        if (sudahDibaca) {
+            System.out.println("Status  : Sudah Dibaca");
+        } else {
+            System.out.println("Status  : Belum Dibaca");
+        }
+        
+        System.out.println();
+    }
 }
