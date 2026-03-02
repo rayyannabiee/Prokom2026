437bcf036e8ae6ae324723695defb8728ff13f1c
Noufalpali <izdiharnoufal15@gmail.com>
2026-03-02 20:10:42 +0700

    Create Utama.java

---
+++ Utama.java
@@ -0,0 +1,16 @@
+import Buku.Daftarbuku;
+import Buku.Detail;
+import Buku.Ketersediaan;
+import Buku.Notbuku;
+
+public class Utama {
+    public static void main(String[] args) throws Exception{
+        Detail b1 = new Daftarbuku("Arah Langkah", "Fiersa Besari", "Sastra Perjalanan", "Mediakita", 2018, 3);
+        Detail bk1 = new Ketersediaan("Arah Langkah", "Fiersa Besari", true);
+
+        Detail[] daftar = {b1, bk1};
+        
+        Notbuku admin = new Notbuku("Mas Pali");
+        admin.cetakStruk(daftar);
+    }
+}
