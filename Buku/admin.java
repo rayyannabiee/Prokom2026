e5303c1b2fdd79f94c0b46bb3257f58a42f3dc3f
rayyannabiee <09021282530096@student.unsri.ac.id>
2026-03-05 22:30:34 +0700

    Create admin.java

---
+++ Buku/admin.java
@@ -0,0 +1,15 @@
+package Buku;
+public class admin {
+    private String username;
+    private String password;
+    
+
+public admin(String username, String password){
+    this.username=username;
+    this.password=password;
+}
+
+public String getusername(){return username;}
+public String getpassword(){return password;}
+
+}
