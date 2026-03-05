0725298f2940451743fd35236c7b95024ec4ae73
bolkiah ayesha <mbolkiah.2007@gmail.com>
2026-03-06 00:57:55 +0700

    Update PerpustakaanGUI.java

--- PerpustakaanGUI.java
+++ PerpustakaanGUI.java
@@ -6,18 +6,15 @@ import javax.swing.border.LineBorder;
 public class PerpustakaanGUI {
     public static void main(String[] args) {
 
-        ImageIcon logo = new ImageIcon("kanata halo.jpg");
+        ImageIcon logo = new ImageIcon("GUI/kanata halo.jpg");
 
-        ImageIcon buku = new ImageIcon("buku.png");
+        ImageIcon buku = new ImageIcon("GUI/buku.png");
 
         JFrame frame = new JFrame();
         frame.setTitle("Perpustakaan Digital");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setResizable(false);
         frame.setSize(480,800);
-        
         frame.setLayout(new BorderLayout());
-                
         frame.setIconImage(logo.getImage());
 
         JPanel panel = new JPanel();
@@ -51,7 +48,47 @@ public class PerpustakaanGUI {
         searchPanel.add(searchBar);
         utama.add(searchPanel, BorderLayout.NORTH);
 
-        frame.add(utama);
+        JPanel containerBuku = new JPanel(new BorderLayout());
+        containerBuku.setBackground(new Color(245, 246, 250));
+        containerBuku.setBorder(new EmptyBorder(0, 15, 20, 15));
+
+        JPanel kartu = new JPanel(new BorderLayout());
+        kartu.setBackground(Color.WHITE);
+        kartu.setBorder(new LineBorder(new Color(230,230,230),1,true));
+
+        JLabel sampulBuku = new JLabel("sampul buku", SwingConstants.CENTER);
+        sampulBuku.setPreferredSize(new Dimension(0,200));
+        sampulBuku.setBorder(new LineBorder(new Color(230,230,230),1,true));
+        kartu.add(sampulBuku, BorderLayout.NORTH);
+
+        JPanel info = new JPanel(new GridLayout(3,1,5,5));
+        info.setBorder(new EmptyBorder(15,15,15,15));
+        info.setOpaque(false);
+
+        JLabel judulBuku = new JLabel("Bumi");
+
+        JLabel pn = new JLabel("Tere Liye • Fantasi");
+        pn.setFont(new Font("Arial",Font.PLAIN,12));
+        pn.setForeground(Color.DARK_GRAY);
+        info.add(pn);
+
+        JButton btnPinjam = new JButton("Pinjam");
+        btnPinjam.setBackground(new Color(33,102,255));
+        btnPinjam.setForeground(Color.WHITE);
+        btnPinjam.setFocusPainted(false);
+
+        info.add(judulBuku);
+        info.add(pn);
+        info.add(btnPinjam);
+
+        kartu.add(info, BorderLayout.CENTER);
+
+        containerBuku.add(kartu, BorderLayout.NORTH);
+
+        utama.add(containerBuku, BorderLayout.CENTER);
+
+        frame.add(utama, BorderLayout.CENTER);
+
         frame.setVisible(true);
 
         
