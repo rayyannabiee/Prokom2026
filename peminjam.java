1ac49446f523d99b4260da4c037368931d833d2b
Noufalpali <izdiharnoufal15@gmail.com>
2026-04-29 09:02:45 +0700

    Update peminjam.java

--- peminjam.java
+++ peminjam.java
@@ -15,7 +15,6 @@ public class peminjam extends manusia {
             System.out.println(" batas pinjam: " + bataspinjam);
     }
     
-<<<<<<< Updated upstream
     private static List<peminjam> daftarpeminjam = new ArrayList<>();
 
     public static boolean tambahpeminjam (peminjam Peminjam){
@@ -99,8 +98,7 @@ public class peminjam extends manusia {
             }
         }
         return null;
+    
     }
 }
-=======
-}
->>>>>>> Stashed changes
+
